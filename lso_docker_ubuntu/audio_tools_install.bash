fb8ba3da40ef39f615c549af0bceeb82bbbcf5c0
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-10-17 18:17:25 -0400

    Add files via upload

---
+++ lso_docker_ubuntu/audio_tools_install.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+echo ""
+echo "==========INSTALLING TOOLS==========="
+if ! [ -x "$(command -v mp3val)" ]
+	then	
+		echo "START MP3VAL INSTALLTION"
+		apt-get update -qq
+		if apt-get install -y -qq mp3val
+			then
+				echo "INSTALLATION SUCCESSFUL"
+				apt-get purge --auto-remove -y
+				apt-get clean				
+			else
+				echo "ERROR: INSTALLTION UNSUCCESSFUL"
+		fi
+	else
+		echo "MP3VAL ALREADY INSTALLED"
+fi
+if ! [ -x "$(command -v flac)" ]
+	then	
+		echo "START FLAC INSTALLTION"
+		apt-get update -qq
+		if apt-get install -y -qq flac
+			then
+				echo "INSTALLATION SUCCESSFUL"
+				apt-get purge --auto-remove -y
+				apt-get clean
+			else
+				echo "ERROR: INSTALLTION UNSUCCESSFUL"
+		fi
+	else
+		echo "FLAC ALREADY INSTALLED"
+fi
+if ! [ -x "$(command -v mp3gain)" ]
+	then
+		echo "START MP3GAIN INSTALLATION"
+		apt-get update -qq
+		apt-get install -y libmpg123-dev
+		echo "**** compile mp3gain ****" && \
+		mkdir -p \
+			/tmp/mp3gain-src && \
+		wget -O /tmp/mp3gain-src/mp3gain.zip https://sourceforge.net/projects/mp3gain/files/mp3gain/1.6.1/mp3gain-1_6_1-src.zip && \
+		cd /tmp/mp3gain-src && \
+		unzip -qq /tmp/mp3gain-src/mp3gain.zip && \
+		sed -i "s#/usr/local/bin#/usr/bin#g" /tmp/mp3gain-src/Makefile && \
+		make && \
+		make install && \
+		echo "**** compile mp3val ****"
+		cd /
+		rm -rf /tmp/mp3gain-src
+		apt-get purge --auto-remove -y
+		apt-get clean
+	else
+		echo "MP3GAIN ALREADY INSTALLED"
+fi
+echo ""
+echo "=====TOOLS INSTALLATION COMPLETE====="
+exit 0
---
+++ lso_docker_ubuntu/mkvtoolnix_install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo ""
+echo "==========INSTALLING TOOLS==========="
+if ! [ -x "$(command -v mkvtoolnix)" ]
+	then
+		echo "Installing mkvtoolnix..."
+		apt-get update -qq
+		if apt-get -y install mkvtoolnix; then
+			echo " "
+			echo "INSTALLATION SUCCESSFUL"
+		else
+			echo " "
+			echo "ERROR: INSTALLTION UNSUCCESSFUL"
+		fi
+		apt-get purge --auto-remove -y
+		apt-get clean
+	else
+		echo "mkvtoolnix already installed"
+fi
+echo ""
+echo "=====TOOLS INSTALLATION COMPLETE====="
+exit 0
