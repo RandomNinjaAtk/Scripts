c63e7cd1ee1b332a6f1d5d05a14a159431435c2d
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-05 16:35:07 -0500

    Update mkvtoolnix_install.bash

--- lso_docker_ubuntu/mkvtoolnix_install.bash
+++ lso_docker_ubuntu/mkvtoolnix_install.bash
@@ -1,17 +1,13 @@
 #!/bin/bash
 echo "==========INSTALLING TOOLS==========="
-if ! [ -x "$(command -v mkvmerge)" ]; then
-	echo "Installing mkvtoolnix..."
-	apt-get update -qq
-	if { apt-get -y install mkvtoolnix; }; then
-		apt-get purge --auto-remove -y
-		apt-get clean
-		echo "INSTALLATION SUCCESSFUL"
-	else
-		echo "ERROR: INSTALLTION UNSUCCESSFUL"
-	fi
+if ! [ -x "$(command -v mkvmerge)" ]; then	
+	echo "INSTALLING mkvtoolnix"
+	apt-get update && \
+	apt-get install -y mkvtoolnix && \		
+	apt-get purge --auto-remove -y && \
+	apt-get clean
 else
-	echo "mkvtoolnix already installed"
+	echo "mkvtoolnix ALREADY INSTALLED"
 fi
 echo "=====TOOLS INSTALLATION COMPLETE====="
 exit 0
