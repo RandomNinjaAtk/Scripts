31f437fbde2a2ed603adf538f38564a3d9f698fe
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-05 17:18:20 -0500

    Update sabnzbd_script_setup.bash

--- lso_docker_ubuntu/sabnzbd_script_setup.bash
+++ lso_docker_ubuntu/sabnzbd_script_setup.bash
@@ -1,20 +1,37 @@
 #!/bin/bash
-if [ ! -d /config/scripts]; then
+echo "setting up script directory"
+if [ ! -d /config/scripts ]; then
   mkdir /config/scripts
+  
+  # Set Permissions
+  echo "setting permissions..."
   chmod 0777 /config/scripts
+  echo "done"
 else
   echo "scripts directory exists"
 fi
 
 # Download Scripts
-if [ ! -f /config/scripts/video-processing.bash]; then
+echo "downloading scripts..."
+if [ ! -f /config/scripts/video-processing.bash ]; then
+  echo "downloading video-processing.bash from: https://github.com/RandomNinjaAtk/Scripts/blob/master/sabnzbd/video-processing.bash"
   curl -o /config/scripts/video-processing.bash https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/sabnzbd/video-processing.bash
+  echo "done"
+
   # Set Permissions
+  echo "setting permissions..."
   chmod +x /config/scripts/video-processing.bash
+  echo "done"
 fi
-if [ ! -f /config/scripts/Deobfuscate.py]; then
+if [ ! -f /config/scripts/Deobfuscate.py ]; then
+  echo "downloading AudioPostProcessing.bash from: https://github.com/RandomNinjaAtk/Scripts/tree/master/sabnzbd/AudioPostProcessing.bash"
   curl -o /config/scripts/AudioPostProcessing.bash https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/sabnzbd/AudioPostProcessing.bash
+  echo "done"
+
   # Set Permissions
+  echo "setting permissions..."
   chmod +x /config/scripts/AudioPostProcessing.bash
+  echo "done"
 fi
+echo "script downloads complete..."
 exit 0
