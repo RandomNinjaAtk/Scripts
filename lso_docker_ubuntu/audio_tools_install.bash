ed65bca034299a5917e832a1c8b5b320d598c277
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-05 16:35:43 -0500

    Update audio_tools_install.bash

--- lso_docker_ubuntu/audio_tools_install.bash
+++ lso_docker_ubuntu/audio_tools_install.bash
@@ -26,27 +26,5 @@ if ! [ -x "$(command -v flac)" ]; then
 else
 	echo "FLAC ALREADY INSTALLED"
 fi
-if ! [ -x "$(command -v mp3gain)" ]; then
-	echo "START MP3GAIN INSTALLATION"
-	apt-get update -qq
-	apt-get install -y libmpg123-dev wget cmake autoconf automake build-essential
-	echo "**** compile mp3gain ****" && \
-	mkdir -p \
-		/tmp/mp3gain-src && \
-	wget -O /tmp/mp3gain-src/mp3gain.zip https://sourceforge.net/projects/mp3gain/files/mp3gain/1.6.1/mp3gain-1_6_1-src.zip && \
-	cd /tmp/mp3gain-src && \
-	unzip -qq /tmp/mp3gain-src/mp3gain.zip && \
-	sed -i "s#/usr/local/bin#/usr/bin#g" /tmp/mp3gain-src/Makefile && \
-	make && \
-	make install && \
-	echo "**** compile mp3val ****"
-	cd /
-	rm -rf /tmp/mp3gain-src
-	apt-get purge --auto-remove -y
-	apt-get clean
-	echo "INSTALLATION SUCCESSFUL"
-else
-	echo "MP3GAIN ALREADY INSTALLED"
-fi
 echo "=====TOOLS INSTALLATION COMPLETE====="
 exit 0
