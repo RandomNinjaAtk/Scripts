a8d56f31903c7d3463714aa75870436596a5738d
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-05 16:36:28 -0500

    Update ffmpeg_install.bash

--- lso_docker_ubuntu/ffmpeg_install.bash
+++ lso_docker_ubuntu/ffmpeg_install.bash
@@ -2,7 +2,7 @@
 echo "==========INSTALLING TOOLS==========="
 if ! [ -x "$(command -v ffmpeg)" ]; then	
 	echo "INSTALLING FFMPEG"
-	apt-get update && \
+	apt-get update -qq && \
 	apt-get install -y ffmpeg && \		
 	apt-get purge --auto-remove -y && \
 	apt-get clean
