469132b9f3abac7ed8d3d6b93a58d945af9fbf7b
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-07 06:05:03 -0500

    Bug fix

--- external/cron_lidarr_jobs.bash
+++ external/cron_lidarr_jobs.bash
@@ -4,7 +4,7 @@ if [ -x "$(command -v crontab)" ]; then
 		echo "job already added..."
 	else
 		echo "adding cron job to crontab..."
-		echo "*/5 * * * *   root   if [ ! -d /config/scripts/lidarr-download-automation/.lidarr-download-automation.exclusivelock ]; then rm /config/scripts/cron-job.log; fi; bash /config/scripts/lidarr-download-automation-start.bash > /config/scripts/cron-job.log"
+		echo "*/5 * * * *   root   if [ ! -d /config/scripts/lidarr-download-automation/.lidarr-download-automation.exclusivelock ]; then rm /config/scripts/cron-job.log; fi; bash /config/scripts/lidarr-download-automation-start.bash > /config/scripts/cron-job.log"  >> "/etc/crontab"
 	fi
 else
 	echo "cron NOT INSTALLED"
