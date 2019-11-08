ba9a0659413da94e7ffdc299b4e9e12c97b60595
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-08 08:03:06 -0500

    Update mp4_automator.bash

--- lso_docker_ubuntu/mp4_automator.bash
+++ lso_docker_ubuntu/mp4_automator.bash
@@ -1,57 +1,62 @@
 #!/bin/bash
-echo "==========INSTALLING TOOLS==========="
+echo "==========INSTALLING SICKBEARD MP4 AUTOMATOR==========="
 
-if ! [ -x "$(command -v ffmpeg)" ]; then	
-	echo "INSTALLING FFMPEG"
-	apt-get update && \
-	apt-get install -y \
-		git \
-		python-pip \
-		openssl \
-		python-dev \
-		libffi-dev \
-		libssl-dev \
-		libxml2-dev \
-		libxslt1-dev \
-		mkvtoolnix \
-		zlib1g-dev
+echo "INSTALLING PRE-REQS"
+apt-get update && \
+apt-get install -y \
+	git \
+	python-pip \
+	openssl \
+	python-dev \
+	libffi-dev \
+	libssl-dev \
+	libxml2-dev \
+	libxslt1-dev \
+	mkvtoolnix \
+	zlib1g-dev
 		
-	apt-get purge --auto-remove -y
-	apt-get clean
+apt-get purge --auto-remove -y
+apt-get clean
+
+mkdir /tmp/ffmpeg
+curl -o /tmp/ffmpeg/ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+cd /tmp/ffmpeg
+tar xvf ffmpeg-git-amd64-static.tar.xz
+find "/usr/bin/" -type f -iname "ffmpeg" -exec rm {} \;
+find "/usr/bin/" -type f -iname "ffprobe" -exec rm {} \;
+find "/tmp/ffmpeg" -type f -iname "ffmpeg" -exec mv {} /usr/bin/ \;
+find "/tmp/ffmpeg" -type f -iname "ffprobe" -exec mv {} /usr/bin/ \;
+cd /
+rm -rf /tmp/ffmpeg
 	
-	mkdir /tmp/ffmpeg
-	curl -o /tmp/ffmpeg/ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-	cd /tmp/ffmpeg
-	tar xvf ffmpeg-git-amd64-static.tar.xz
-	find "/usr/bin/" -type f -iname "ffmpeg" -exec rm {} \;
-	find "/usr/bin/" -type f -iname "ffprobe" -exec rm {} \;
-	find "/tmp/ffmpeg" -type f -iname "ffmpeg" -exec mv {} /usr/bin/ \;
-	find "/tmp/ffmpeg" -type f -iname "ffprobe" -exec mv {} /usr/bin/ \;
-	cd /
-	rm -rf /tmp/ffmpeg
-	
- 	echo "INSTALLING PIP TOOLS"
-	pip install --no-cache-dir -U \
-		requests \
-		requests[security] \
-		requests-cache \
-		babelfish \
-		"guessit<2" \
-		"subliminal<2" \
-		stevedore==1.19.1 \
-		python-dateutil \
-		qtfaststart
-		
-	echo "DOWNLOAD SICKBEARD_MP4_AUTOMATOR"
+echo "INSTALLING PIP TOOLS"
+pip install --no-cache-dir -U \
+	requests \
+	requests[security] \
+	requests-cache \
+	babelfish \
+	"guessit<2" \
+	"subliminal<2" \
+	stevedore==1.19.1 \
+	python-dateutil \
+	qtfaststart
+
+if [ -f /config/scripts/sickbeard_mp4_automator/autoProcess.ini ]; then
+	mv /config/scripts/sickbeard_mp4_automator/autoProcess.ini /config/scripts/ && \
+	echo "DOWNLOAD SICKBEARD_MP4_AUTOMATOR" && \
 	git clone git://github.com/mdhiggins/sickbeard_mp4_automator.git /config/scripts/sickbeard_mp4_automator/ && \
 	touch /config/scripts/sickbeard_mp4_automator/info.log && \
+	mv /config/scripts/autoProcess.ini /config/scripts/sickbeard_mp4_automator/
 	chmod a+rwx -R /config/scripts/sickbeard_mp4_automator && \
-
-	rm -rf \
-		/tmp/* \
-		/var/lib/apt/lists/* \
-		/var/tmp/*
+	echo "DONE"
+else
+	echo "DOWNLOAD SICKBEARD_MP4_AUTOMATOR" && \
+	git clone git://github.com/mdhiggins/sickbeard_mp4_automator.git /config/scripts/sickbeard_mp4_automator/ && \
+	touch /config/scripts/sickbeard_mp4_automator/info.log && \
+	mv /config/scripts/autoProcess.ini /config/scripts/sickbeard_mp4_automator/
+	chmod a+rwx -R /config/scripts/sickbeard_mp4_automator && \
+	echo "DONE"
 fi
 
-echo "=====TOOLS INSTALLATION COMPLETE====="
+echo "=====SICKBEARD MP4 AUTOMATOR INSTALLATION COMPLETE====="
 exit 0
