f98042f00f2d9477b6d8dc72017e89bcdc85aea8
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-09 20:53:44 -0500

    Update mp4-video-processing.bash

--- sabnzbd/mp4-video-processing.bash
+++ sabnzbd/mp4-video-processing.bash
@@ -2,19 +2,18 @@
 
 # Download Scripts
 
-if [ ! -f /config/scripts/Deobfuscate.py ]; then
+if [ ! -f /config/scripts/sickbeard_mp4_automator/Deobfuscate.py ]; then
     echo "downloading Deobfuscate.py from: https://github.com/sabnzbd/sabnzbd/blob/develop/scripts/Deobfuscate.py"
-    curl -o /config/scripts/Deobfuscate.py https://raw.githubusercontent.com/sabnzbd/sabnzbd/develop/scripts/Deobfuscate.py
+    curl -o /config/scripts/sickbeard_mp4_automator/Deobfuscate.py https://raw.githubusercontent.com/sabnzbd/sabnzbd/develop/scripts/Deobfuscate.py
     echo "done"
 
     # Set Permissions
     echo "setting permissions..."
-    chmod +x /config/scripts/Deobfuscate.py
+    chmod +x /config/scripts/sickbeard_mp4_automator/Deobfuscate.py
     echo "done"
 fi
 
 # Execute on new downloads
-cd /config/scripts
-timeout --foreground 1m python Deobfuscate.py
 cd /config/scripts/sickbeard_mp4_automator
+timeout --foreground 1m python Deobfuscate.py
 python manual.py -i "$1" -nt
