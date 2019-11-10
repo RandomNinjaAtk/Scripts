056cff26a1192ee9b38f42d1249a47bb34a156ea
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-10 10:16:23 -0500

    Update mp4_automator.bash

--- lso_docker_ubuntu/mp4_automator.bash
+++ lso_docker_ubuntu/mp4_automator.bash
@@ -1,48 +1,62 @@
 #!/bin/bash
 echo "==========INSTALLING SICKBEARD MP4 AUTOMATOR==========="
 
-echo "INSTALLING PRE-REQS"
-apt-get update && \
-apt-get install -y \
-	git \
-	python-pip \
-	openssl \
-	python-dev \
-	libffi-dev \
-	libssl-dev \
-	libxml2-dev \
-	libxslt1-dev \
-	mkvtoolnix \
-	zlib1g-dev
-		
-apt-get purge --auto-remove -y
-apt-get clean
+if ! [ -x "$(command -v mkvmerge)" ]; then
+	echo "INSTALLING PRE-REQS"
+	apt-get update && \
+	apt-get install -y \
+		git \
+		openssl \
+		python-dev \
+		libffi-dev \
+		libssl-dev \
+		libxml2-dev \
+		libxslt1-dev \
+		mkvtoolnix \
+		zlib1g-dev
+
+	apt-get purge --auto-remove -y && \
+	apt-get clean
+fi
 
 if ! [ -x "$(command -v ffmpeg)" ]; then
 	if [ -f /config/custom-cont-init.d/ffmpeg_install.bash ]; then
+		echo "running ffmpeg_install.bash..." && \
+		bash /config/custom-cont-init.d/ffmpeg_install.bash && \
+		rm /config/custom-cont-init.d/ffmpeg_install.bash
+	else
+		echo "downloading ffmpeg_install.bash from: https://github.com/RandomNinjaAtk/Scripts/blob/master/lso_docker_ubuntu/ffmpeg_install.bash" && \
+		curl -o /config/custom-cont-init.d/ffmpeg_install.bash https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/lso_docker_ubuntu/ffmpeg_install.bash && \
+		echo "done" && \
+		echo "running ffmpeg_install.bash..." && \
+		bash /config/custom-cont-init.d/ffmpeg_install.bash && \
 		rm /config/custom-cont-init.d/ffmpeg_install.bash
 	fi
-	echo "downloading ffmpeg_install.bash from: https://github.com/RandomNinjaAtk/Scripts/blob/master/lso_docker_ubuntu/ffmpeg_install.bash"
-	curl -o /config/custom-cont-init.d/ffmpeg_install.bash https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/lso_docker_ubuntu/ffmpeg_install.bash
-	echo "done"
-	echo "running ffmpeg_install.bash..."
-	bash /config/custom-cont-init.d/ffmpeg_install.bash
 fi
-	
-echo "INSTALLING PIP TOOLS"
-pip install --no-cache-dir -U \
-	requests \
-	requests[security] \
-	requests-cache \
-	babelfish \
-	"guessit<2" \
-	"subliminal<2" \
-	stevedore==1.19.1 \
-	python-dateutil \
-	qtfaststart
+
+if ! [ -x "$(command -v pip)" ]; then
+	echo "INSTALLING PIP TOOLS" && \
+		apt-get update && \
+		apt-get install -y \
+			python-pip
+
+		apt-get purge --auto-remove -y && \
+		apt-get clean
+		
+	pip install --no-cache-dir -U \
+		requests \
+		requests[security] \
+		requests-cache \
+		babelfish \
+		"guessit<2" \
+		"subliminal<2" \
+		stevedore==1.19.1 \
+		python-dateutil \
+		qtfaststart
+fi
 
 if [ -f /config/scripts/sickbeard_mp4_automator/autoProcess.ini ]; then
-	echo "Backup autoProcess.ini configuration"
+	echo "Backup autoProcess.ini configuration" && \
 	mv /config/scripts/sickbeard_mp4_automator/autoProcess.ini /config/scripts/ && \
 	echo "DOWNLOAD CURRENT SICKBEARD_MP4_AUTOMATOR" && \
 	rm -rf /config/scripts/sickbeard_mp4_automator && \
@@ -54,24 +68,24 @@ if [ -f /config/scripts/sickbeard_mp4_automator/autoProcess.ini ]; then
 	if [ -d /var/log/sickbeard_mp4_automator ]; then
 		rm -rf /var/log/sickbeard_mp4_automator
 	fi
-	mkdir /var/log/sickbeard_mp4_automator
-	chmod 0777 -R /var/log/sickbeard_mp4_automator
+	mkdir /var/log/sickbeard_mp4_automator && \
+	chmod 0777 -R /var/log/sickbeard_mp4_automator && \
 	ln -s /config/scripts/sickbeard_mp4_automator/index.log /var/log/sickbeard_mp4_automator/index.log
 	echo "DONE"
 else
 	echo "DOWNLOAD CURRENT SICKBEARD_MP4_AUTOMATOR" && \
 	if [ -d /config/scripts/sickbeard_mp4_automator ]; then
-		rm -rf /config/scripts/sickbeard_mp4_automator
+		rm -rf /config/scripts/sickbeard_mp4_automator && \
 	fi
 	git clone git://github.com/mdhiggins/sickbeard_mp4_automator.git /config/scripts/sickbeard_mp4_automator/ && \
 	touch /config/scripts/sickbeard_mp4_automator/index.log && \
 	chmod 0777 -R /config/scripts/sickbeard_mp4_automator && \
 	if [ -d /var/log/sickbeard_mp4_automator ]; then
-		rm -rf /var/log/sickbeard_mp4_automator
+		rm -rf /var/log/sickbeard_mp4_automator && \
 	fi
-	mkdir /var/log/sickbeard_mp4_automator
-	chmod 0777 -R /var/log/sickbeard_mp4_automator
-	ln -s /config/scripts/sickbeard_mp4_automator/index.log /var/log/sickbeard_mp4_automator/index.log
+	mkdir /var/log/sickbeard_mp4_automator && \
+	chmod 0777 -R /var/log/sickbeard_mp4_automator && \
+	ln -s /config/scripts/sickbeard_mp4_automator/index.log /var/log/sickbeard_mp4_automator/index.log && \
 	echo "DONE"
 fi
 
