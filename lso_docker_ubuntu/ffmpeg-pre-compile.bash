056e9614f4f79158774ba4abb585c309582c68a3
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-11 13:46:42 -0500

    Update ffmpeg-pre-compile.bash

--- lso_docker_ubuntu/ffmpeg-pre-compile.bash
+++ lso_docker_ubuntu/ffmpeg-pre-compile.bash
@@ -4,9 +4,7 @@ echo "==========INSTALLING PRE-COMPILED FFMPEG==========="
 if ! [ -x "$(command -v ffmpeg)" ]; then	
 	echo "INSTALLING FFMPEG"
 	apt-get update && \
-	apt-get install -y ffmpeg && \		
-	apt-get purge --auto-remove -y && \
-	apt-get clean
+	apt-get install -y
 else
 	echo "FFMPEG ALREADY INSTALLED"
 fi
