6d101dafd8cd6d9a44cc57fb66c6bc6e25570d78
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-10 19:33:55 -0500

    Update mp4-video-processing.bash

--- sabnzbd/mp4-video-processing.bash
+++ sabnzbd/mp4-video-processing.bash
@@ -15,6 +15,20 @@ fi
 
 # Execute on new downloads
 cd /config/scripts/sickbeard_mp4_automator
+
+# Run sabnzbd Deobfuscation script
 timeout --foreground 1m python Deobfuscate.py
+
+# Check for video files, if none found error out
+if find "$1" -type f -iregex ".*/.*\.\(ts\|\mpeg\|mov\|flv\|ogg\|mkv\|mp4\|avi\)" | read; then
+	echo "REMOVE NON VIDEO FILES"
+	find "$1"/* -type f -not -iregex ".*/.*\.\(webvtt\|ass\|srt\|ts\|mpeg\|mov\|flv\|ogg\|mkv\|mp4\|avi\)" -delete
+	echo "REMOVE NON VIDEO FILES COMPLETE"
+else
+	echo "ERROR: NO VIDEO FILES FOUND"
+	exit 1
+fi
+
+# Manual run of Sickbeard MP4 Automator
 python manual.py -i "$1" -nt
 echo "COMPLETE"
