9e85e144950719ad7456dd4466daa7b695ae3c9a
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-11 14:04:47 -0500

    Create ffmpeg-repo-installer.bash

---
+++ lso_docker_ubuntu/ffmpeg-repo-installer.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "==========INSTALLING REPO FFMPEG==========="
+
+if ! [ -x "$(command -v ffmpeg)" ]; then
+	echo "INSTALLING FFMPEG"
+	apt-get update && \
+	apt-get install -y ffmpeg
+else
+	echo "FFMPEG ALREADY INSTALLED"
+fi
+
+echo "=====REPO FFMPEG INSTALLATION COMPLETE====="
+exit 0
