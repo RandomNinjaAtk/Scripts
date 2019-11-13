1f3248791aaded06ba27801458027832829cf1f1
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-13 08:25:37 -0500

    Update ffmpeg-pre-compile.bash

--- lso_docker_ubuntu/ffmpeg-pre-compile.bash
+++ lso_docker_ubuntu/ffmpeg-pre-compile.bash
@@ -3,22 +3,20 @@ echo "==========INSTALLING PRE-COMPILED FFMPEG==========="
 
 if ! [ -x "$(command -v ffmpeg)" ]; then
 	echo "INSTALLING FFMPEG"
-	apt-get update && \
-	apt-get install -y ffmpeg xz-utils
+
+mkdir /tmp/ffmpeg
+	curl -o /tmp/ffmpeg/ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+	cd /tmp/ffmpeg
+	tar xvf ffmpeg-git-amd64-static.tar.xz
+	find "/usr/bin/" -type f -iname "ffmpeg" -exec rm {} \;
+	find "/usr/bin/" -type f -iname "ffprobe" -exec rm {} \;
+	find "/tmp/ffmpeg" -type f -iname "ffmpeg" -exec mv {} /usr/bin/ \;
+	find "/tmp/ffmpeg" -type f -iname "ffprobe" -exec mv {} /usr/bin/ \;
+	cd /
+	rm -rf /tmp/ffmpeg
 else
 	echo "FFMPEG ALREADY INSTALLED"
 fi
-
-mkdir /tmp/ffmpeg
-curl -o /tmp/ffmpeg/ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-cd /tmp/ffmpeg
-tar xvf ffmpeg-git-amd64-static.tar.xz
-find "/usr/bin/" -type f -iname "ffmpeg" -exec rm {} \;
-find "/usr/bin/" -type f -iname "ffprobe" -exec rm {} \;
-find "/tmp/ffmpeg" -type f -iname "ffmpeg" -exec mv {} /usr/bin/ \;
-find "/tmp/ffmpeg" -type f -iname "ffprobe" -exec mv {} /usr/bin/ \;
-cd /
-rm -rf /tmp/ffmpeg
 
 echo "=====PRE-COMPILED FFMPEG INSTALLATION COMPLETE====="
 exit 0
