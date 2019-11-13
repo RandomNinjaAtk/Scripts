c079220aa170ec1e2987553245737aa8238eda29
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-13 08:38:18 -0500

    Update ffmpeg-pre-compile.bash

--- lso_docker_ubuntu/ffmpeg-pre-compile.bash
+++ lso_docker_ubuntu/ffmpeg-pre-compile.bash
@@ -3,7 +3,9 @@ echo "==========INSTALLING PRE-COMPILED FFMPEG==========="
 
 if ! [ -x "$(command -v ffmpeg)" ]; then
 	echo "INSTALLING FFMPEG"
-
+	apt-get update -qq && \
+	apt-get install -y xz-utils
+	
 	mkdir /tmp/ffmpeg
 	curl -o /tmp/ffmpeg/ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
 	cd /tmp/ffmpeg
