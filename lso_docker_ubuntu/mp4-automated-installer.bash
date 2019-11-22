0a9d62a5f49ca65ae234725fcd67b9a327685944
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-11-22 18:14:27 -0500

    Consildation of scripts for simplification

--- lso_docker_ubuntu/mp4-automated-installer.bash
+++ lso_docker_ubuntu/mp4-automated-installer.bash
@@ -1,21 +1,74 @@
 #!/bin/bash
 echo "==========MP4 AUTOMATOR INSTALLER==========="
+echo ""
+echo ""
+echo "===INSTALLING PRE-REQS==="
 
-if [ -f /config/custom-cont-init.d/mp4_automator.bash ]; then
-	bash /config/custom-cont-init.d/mp4_automator.bash && \
-	rm /config/custom-cont-init.d/mp4_automator.bash
-else 
-	echo "downloading ffmpeg_install.bash from: https://github.com/RandomNinjaAtk/Scripts/blob/master/lso_docker_ubuntu/mp4_automator.bash"  && \
-	curl -o /config/custom-cont-init.d/mp4_automator.bash https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/lso_docker_ubuntu/mp4_automator.bash  && \
-	echo "done" && \
-	echo "running mp4_automator.bash..." && \
-	bash /config/custom-cont-init.d/mp4_automator.bash && \
-	rm /config/custom-cont-init.d/mp4_automator.bash
+apt-get update -qq && \
+apt-get install -qq -y \
+	git \
+	openssl \
+	python-dev \
+	libffi-dev \
+	libssl-dev \
+	libxml2-dev \
+	libxslt1-dev \
+	mkvtoolnix \
+	zlib1g-dev \
+	ffmpeg \
+	python-pip
+
+apt-get purge --auto-remove -y && \
+apt-get clean
+
+pip install --no-cache-dir -U \
+	requests \
+	requests[security] \
+	requests-cache \
+	babelfish \
+	"guessit<2" \
+	"subliminal<2" \
+	stevedore==1.19.1 \
+	python-dateutil \
+	qtfaststart \
+	tmdbsimple
+	
+echo ""
+echo ""
+echo "=========COMPLETE========"
+echo ""
+echo "===CHECKING FOR EXISTING CONFIG==="
+if [ -f /config/scripts/sickbeard_mp4_automator/autoProcess.ini ]; then
+	echo "Config found..." && \
+	echo "Backup autoProcess.ini configuration..." && \
+	mv /config/scripts/sickbeard_mp4_automator/autoProcess.ini /config/scripts/
+else
+	echo "No config found..."
+fi
+echo ""
+echo "===INSTALLING & UPDATING SICKBEARD AUTOMATOR==="
+if [ -d /config/scripts/sickbeard_mp4_automator ]; then
+	echo "Removing previous installation..."
+	rm -rf /config/scripts/sickbeard_mp4_automator
+fi
+echo "Downloading sickbeared_mp4_automator..." && \
+git clone git://github.com/mdhiggins/sickbeard_mp4_automator.git /config/scripts/sickbeard_mp4_automator/ && \
+touch /config/scripts/sickbeard_mp4_automator/index.log && \
+chmod 0777 -R /config/scripts/sickbeard_mp4_automator
+if [ ! -d /var/log/sickbeard_mp4_automator ]; then
+	mkdir /var/log/sickbeard_mp4_automator
+fi
+rm -rf /var/log/sickbeard_mp4_automator/* && \
+chmod 0777 -R /var/log/sickbeard_mp4_automator && \
+ln -s /config/scripts/sickbeard_mp4_automator/index.log /var/log/sickbeard_mp4_automator/index.log  && \
+if [ -f /config/scripts/autoProcess.ini ]; then
+	echo "Restoring backup (autoProcess.ini) configuration..." && \
+	mv /config/scripts/autoProcess.ini /config/scripts/sickbeard_mp4_automator/
 fi
 
-
 if [ ! -f /config/scripts/sickbeard_mp4_automator/autoProcess.ini ]; then
-	echo "downloading config from: https://github.com/RandomNinjaAtk/Scripts/blob/master/config/autoProcess.ini" && \
+	echo "New installation dectected..."
+	echo "Downloading config from: https://github.com/RandomNinjaAtk/Scripts/blob/master/config/autoProcess.ini" && \
 	curl -o /config/scripts/sickbeard_mp4_automator/autoProcess.ini https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/config/autoProcess.ini && \
 	echo "done" && \
 	chmod 0666 /config/scripts/sickbeard_mp4_automator/autoProcess.ini
@@ -27,6 +80,7 @@ if [ -f /config/config.xml ]; then
 	if grep -q 7878 /config/config.xml; then
 		if [ -f /config/scripts/sickbeard_mp4_automator/autoProcess.ini ]; then
 			if grep -q radarrkey /config/scripts/sickbeard_mp4_automator/autoProcess.ini; then
+				echo "Radarr installation detected..."
 				echo "Updating config with radarr api key..."
 				search="radarrkey"
 				apikey="$(grep "<ApiKey>" /config/config.xml | sed "s/\  <ApiKey>//;s/<\/ApiKey>//")"
@@ -40,6 +94,7 @@ if [ -f /config/config.xml ]; then
 	if grep -q 8989 /config/config.xml; then
 		if [ -f /config/scripts/sickbeard_mp4_automator/autoProcess.ini ]; then
 			if grep -q sonarrkey /config/scripts/sickbeard_mp4_automator/autoProcess.ini; then
+				echo "Sonarr installation detected..."
 				echo "Updating config with sonarr api key..."
 				search="sonarrkey"
 				apikey="$(grep "<ApiKey>" /config/config.xml | sed "s/\  <ApiKey>//;s/<\/ApiKey>//")"
@@ -77,5 +132,5 @@ else
 	fi
 fi
 
-echo "=====MP4 AUTOMATOR INSTALLER SETUP COMPLETE====="
+echo "=====MP4 AUTOMATOR INSTALLATION COMPLETE====="
 exit 0
