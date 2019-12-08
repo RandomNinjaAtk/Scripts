32df5c47daf4c2432b9a16337a66eeb60abd583c
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-08 09:27:32 -0500

    Removed alac -> other format conversion, fixed flac -> alac conversion

--- sabnzbd/AudioPostProcessing.bash
+++ sabnzbd/AudioPostProcessing.bash
@@ -9,7 +9,7 @@
 RemoveNonAudioFiles="TRUE" # TURE = ENABLED, Deletes non FLAC/M4A/MP3/OPUS/OGG files
 DuplicateFileCleanUp="TRUE" # TRUE = ENABLED, Deletes duplicate files
 AudioVerification="TRUE" # TRUE = ENABLED, Verifies FLAC/MP3 files for errors (fixes MP3's, deletes bad FLAC files)
-Convert="FALSE" # TRUE = ENABLED, Only converts lossless FLAC/ALAC files
+Convert="FALSE" # TRUE = ENABLED, Only converts lossless FLAC files
 ConversionFormat="MP3" # SET TO: OPUS or AAC or MP3 or FLAC or ALAC - converts lossless FLAC files to set format
 Threads="0" # SET TO: "0" to use maximum number of threads for multi-threaded operations
 ReplaygainTagging="TRUE" # TRUE = ENABLED, adds replaygain tags for compatible players (FLAC ONLY)
@@ -63,7 +63,7 @@ conversion () {
 		fi
 		if [ "${ConversionFormat}" = AAC ]; then
 			echo "AAC CONVERSION START"
-			if { find "$1" -type f -iregex ".*/.*\.\(flac\)" | sed -e 's/.flac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.flac\" -n -vn -acodec libfdk_aac -ab 320k -movflags faststart \"@.m4a\" && echo \"CONVERSION SUCCESS: @.m4a\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
+			if { find "$1" -type f -iregex ".*/.*\.\(flac\)" | sed -e 's/.flac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.flac\" -n -vn -acodec aac -ab 320k -movflags faststart \"@.m4a\" && echo \"CONVERSION SUCCESS: @.m4a\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
 				echo "AAC CONVERSION COMPLETE"
 			else
 				echo "ERROR: AAC CONVERSION FAILED" && exit 1
@@ -87,56 +87,14 @@ conversion () {
 		fi
 		if [ "${ConversionFormat}" = ALAC ]; then
 			echo "ALAC CONVERSION START"
-			if { find "${DownloadDir}/files/" -name "*.flac" | sed -e 's/.flac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.flac\" -n -vn -acodec alac -movflags faststart \"@.m4a\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
-				echo "ALAC CONVERSION COMPLETE"
-			else
-				echo "ERROR: ALAC CONVERSION FAILED" && exit 1
-			fi
-		fi
-	elif find "$1" -type f -iregex ".*/.*\.\(alac\)" | read; then
-		echo "ALAC FILES FOUND"
-		if [ "${ConversionFormat}" = OPUS ]; then
-			echo "OPUS CONVERSION START"
-			if { find "$1" -type f -iregex ".*/.*\.\(alac\)" | sed -e 's/.alac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.alac\" -n -vn -acodec libopus -ab 160k -application audio \"@.opus\" && echo \"CONVERSION SUCCESS: @.opus\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
-				echo "OPUS CONVERSION COMPLETE"
-			else
-				echo "ERROR: OPUS CONVERSION FAILED" && exit 1
-			fi
-		fi
-		if [ "${ConversionFormat}" = AAC ]; then
-			echo "AAC CONVERSION START"
-			if { find "$1" -type f -iregex ".*/.*\.\(alac\)" | sed -e 's/.alac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.alac\" -n -vn -acodec libfdk_aac -ab 320k -movflags faststart \"@.m4a\" && echo \"CONVERSION SUCCESS: @.m4a\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
-				echo "AAC CONVERSION COMPLETE"
-			else
-				echo "ERROR: AAC CONVERSION FAILED" && exit 1
-			fi
-		fi
-		if [ "${ConversionFormat}" = MP3 ]; then
-			echo "MP3 CONVERSION START"
-			if { find "$1" -type f -iregex ".*/.*\.\(alac\)" | sed -e 's/.alac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.alac\" -n -vn -acodec libmp3lame -ab 320k \"@.mp3\" && echo \"CONVERSION SUCCESS: @.mp3\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
-				echo "MP3 CONVERSION COMPLETE"
-			else
-				echo "ERROR: MP3 CONVERSION FAILED" && exit 1
-			fi
-		fi
-		if [ "${ConversionFormat}" = FLAC ]; then
-			echo "FLAC CONVERSION START"
-			if { find "$1" -type f -iregex ".*/.*\.\(alac\)" | sed -e 's/.alac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.alac\" -n -vn -acodec flac \"@.temp.flac\" && echo \"CONVERSION SUCCESS: @.flac\" && rm \"@.flac\" && mv \"@.temp.flac\" \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
-				echo "FLAC CONVERSION COMPLETE"
-			else
-				echo "ERROR: FLAC CONVERSION FAILED" && exit 1
-			fi
-		fi
-		if [ "${ConversionFormat}" = ALAC ]; then
-			echo "ALAC CONVERSION START"
-			if { find "${DownloadDir}/files/" -name "*.flac" | sed -e 's/.flac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.flac\" -n -vn -acodec alac -movflags faststart \"@.m4a\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
+			if { find "$1" -type f -iregex ".*/.*\.\(flac\)" | sed -e 's/.flac$//' -e "s/'/\\'/g" -e 's/\$/\\$/g' | xargs -d '\n' -n1 -I@ -P ${Threads} bash -c "ffmpeg -loglevel warning -hide_banner -stats -i \"@.flac\" -n -vn --acodec aac -ab 320k -movflags faststart \"@.m4a\" && echo \"CONVERSION SUCCESS: @.m4a\" && rm \"@.flac\" && echo \"SOURCE FILE DELETED: @.flac\""; }; then
 				echo "ALAC CONVERSION COMPLETE"
 			else
 				echo "ERROR: ALAC CONVERSION FAILED" && exit 1
 			fi
 		fi
 	else
-		echo "No lossless (FLAC/ALAC) files found to convert"
+		echo "No lossless (FLAC) files found to convert"
 	fi
 }
 
