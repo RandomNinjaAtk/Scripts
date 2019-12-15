6db851872737701a8d3e66bf8b0a69e36b3eb941
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-15 11:30:28 -0500

    Update import.bash

--- external/import.bash
+++ external/import.bash
@@ -14,7 +14,7 @@ if [ "$(ls -A "${DownloadDir}")" ]; then
 		if find "$dir" -type f -name "*.MATCHED.*" -mindepth 1 -maxdepth 1 | read; then
 			logit "Matched with beets!"
 		else
-			echo "Unable to match using beets to a musicbrainz relase, deleting..."
+			logit "Unable to match using beets to a musicbrainz relase, deleting..."
 			rm -rf "$dir"
 		fi
 	done
