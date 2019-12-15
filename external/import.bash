ab1e9b6efde6f83598caa882e300544f0d1d95da
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-15 11:36:15 -0500

    Update import.bash

--- external/import.bash
+++ external/import.bash
@@ -8,13 +8,13 @@ if [ "$(ls -A "${DownloadDir}")" ]; then
 		rm /config/scripts/beets/beets.log
 		sleep 1s
 	fi
-	dlloc=($(find "${DownloadDir}" -type d -mindepth 1 -maxdepth 1 -newer "${DownloadDir}/temp-hold"))
+	dlloc=($(find "${DownloadDir}" -type d -mindepth 1 -newer "${DownloadDir}/temp-hold"))
 	for dir in "${dlloc[@]}"; do
 		beet -c /config/scripts/beets/config.xml -d "$dir" import -q "$dir"
-		if find "$dir" -type f -name "*.MATCHED.*" -mindepth 1 -maxdepth 1 | read; then
+		if find "$dir" -type f -iname "*.MATCHED.*" | read; then
 			logit "Matched with beets!"
 		else
-			logit "Unable to match using beets to a musicbrainz relase, deleting..."
+			echo "Unable to match using beets to a musicbrainz relase, deleting..."
 			rm -rf "$dir"
 		fi
 	done
