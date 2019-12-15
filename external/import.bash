82a23a5a5d95ead37ff84ca94c98cf95e2f16c2a
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-15 10:57:50 -0500

    Update import.bash

--- external/import.bash
+++ external/import.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ "$(ls -A "${DownloadDir}")" ]; then
-    rm /config/scripts/beets/library.blb
-    rm /config/scripts/beets/beets.log
+	rm /config/scripts/beets/library.blb
+	rm /config/scripts/beets/beets.log
 	sleep 1s
-    dlloc=($(find "${DownloadDir}" -type d -mindepth 1 -maxdepth 1 ! -newer "${DownloadDir}/temp-hold"))
-    for dir in "${dlloc[@]}"; do
-        beet -c /config/scripts/beets/config.xml -d "$dir" import -q "$dir"
-        if find "$dir" -type f -name "*.MATCHED.*" -mindepth 1 -maxdepth 1 | read; then
-            logit "Matched with beets!"
-        else
-            echo "Unable to match using beets to a musicbrainz relase, deleting..."
-            rm -rf "$dir"
-        fi
-    done
+	dlloc=($(find "${DownloadDir}" -type d -mindepth 1 -maxdepth 1 ! -newer "${DownloadDir}/temp-hold"))
+	for dir in "${dlloc[@]}"; do
+		beet -c /config/scripts/beets/config.xml -d "$dir" import -q "$dir"
+		if find "$dir" -type f -name "*.MATCHED.*" -mindepth 1 -maxdepth 1 | read; then
+			logit "Matched with beets!"
+		else
+			echo "Unable to match using beets to a musicbrainz relase, deleting..."
+			rm -rf "$dir"
+		fi
+	done
 fi
