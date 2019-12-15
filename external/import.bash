d2e04a22f7b8edc05f2788c56d5de9cd1d63380e
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-15 12:52:51 -0500

    Update import.bash

--- external/import.bash
+++ external/import.bash
@@ -11,11 +11,12 @@ if [ "$(ls -A "${DownloadDir}")" ]; then
 	fi
 	beets=($(find "${DownloadDir}" -type f -iregex ".*/.*\.\(flac\|opus\|m4a\|mp3\)" -newer "${DownloadDir}/temp-hold" -printf '%h\n' | sed -e "s/'/\\'/g" -e 's/\$/\$/g' | sort -u))
 	for beetdir in "${beets[@]}"; do
+		logit "Attempting to match using beets..."
 		beet -c /config/scripts/beets/config.yaml -d "${beetdir}" import -q "${beetdir}"
 		if find "${beetdir}" -type f -iname "*.MATCHED.*" | read; then
-			logit "Matched with beets!"
+			logit "SUCCESS: Matched with beets!"
 		else
-			logit "Unable to match using beets to a musicbrainz release, deleting..."
+			logit "ERROR: Unable to match using beets to a musicbrainz release, deleting..."
 			rm -rf "${beetdir}"
 		fi
 	done
