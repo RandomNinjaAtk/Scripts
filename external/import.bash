a55f504ac3b3d4cf1ef32ab61b3393fca17019be
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-15 12:25:57 -0500

    Update import.bash

--- external/import.bash
+++ external/import.bash
@@ -12,7 +12,7 @@ if [ "$(ls -A "${DownloadDir}")" ]; then
 	beets=($(find "${DownloadDir}" -type f -iregex ".*/.*\.\(flac\|opus\|m4a\|mp3\)" -newer "${DownloadDir}/temp-hold" -printf '%h\n' | sed -e "s/'/\\'/g" -e 's/\$/\$/g' | sort -u))
 	for beetdir in "${beets[@]}"; do
 		beet -c /config/scripts/beets/config.yaml -d "${beetdir}" import -q "${beetdir}"
-		if find "$dir" -type f -iname "*.MATCHED.*" | read; then
+		if find "${beetdir}" -type f -iname "*.MATCHED.*" | read; then
 			logit "Matched with beets!"
 		else
 			logit "Unable to match using beets to a musicbrainz relase, deleting..."
