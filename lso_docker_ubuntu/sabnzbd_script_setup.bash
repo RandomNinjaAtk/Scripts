4c894a1360c6abbb20291c519dfccf42cb03b70b
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-16 07:05:29 -0500

    Update sabnzbd_script_setup.bash

--- lso_docker_ubuntu/sabnzbd_script_setup.bash
+++ lso_docker_ubuntu/sabnzbd_script_setup.bash
@@ -1,14 +1,64 @@
 #!/bin/bash
-echo "setting up script directory"
-if [ ! -d /config/scripts ]; then
-  mkdir /config/scripts
-  
-  # Set Permissions
-  echo "setting permissions..."
-  chmod 0777 /config/scripts
-  echo "done"
+
+echo "INSTALLING REQUIREMENTS"
+apt-get update -qq && \
+apt-get install -qq -y \
+	mkvtoolnix \
+	mp3val \
+	flac \
+	wget \
+	nano \
+	unzip \
+	python-dev \
+	python-pip && \
+apt-get purge --auto-remove -y && \
+apt-get clean
+
+pip install --no-cache-dir -U \
+	beets
+	
+if ! [ -x "$(command -v ffmpeg)" ]; then
+	echo "INSTALLING FFMPEG"
+	apt-get update -qq && \
+	apt-get install -y xz-utils
+	
+	mkdir /tmp/ffmpeg
+	curl -o /tmp/ffmpeg/ffmpeg-git-amd64-static.tar.xz https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+	cd /tmp/ffmpeg
+	tar xvf ffmpeg-git-amd64-static.tar.xz
+	find "/usr/bin/" -type f -iname "ffmpeg" -exec rm {} \;
+	find "/usr/bin/" -type f -iname "ffprobe" -exec rm {} \;
+	find "/tmp/ffmpeg" -type f -iname "ffmpeg" -exec mv {} /usr/bin/ \;
+	find "/tmp/ffmpeg" -type f -iname "ffprobe" -exec mv {} /usr/bin/ \;
+	cd /
+	rm -rf /tmp/ffmpeg
 else
-  echo "scripts directory exists"
+	echo "FFMPEG ALREADY INSTALLED"
+fi
+
+if [ ! -d /config/scripts ]; then
+	echo "setting up script directory"
+	mkdir /config/scripts
+	# Set Permissions
+	echo "setting permissions..."
+	chmod 0777 /config/scripts
+	echo "done"
+fi
+
+if [ ! -d /config/scripts/beets ]; then
+	mkdir /config/scripts/beets
+fi
+
+
+if [ -f /config/scripts/beets/config.xml ]; then
+	rm /config/scripts/beets/config.xml
+	sleep 1s
+fi
+if [ ! -f /config/scripts/beets/config.xml ]; then
+	echo "downloading config.yaml from: https://github.com/RandomNinjaAtk/Scripts/blob/master/config/config.yaml"
+	curl -o /config/scripts/beets/config.yaml https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/config/config.yaml
+	echo "done"
+	chmod 0666 /config/scripts/beets/config.yaml
 fi
 
 # Download Scripts
@@ -20,7 +70,7 @@ if [ ! -f /config/scripts/video-processing.bash ]; then
 
   # Set Permissions
   echo "setting permissions..."
-  chmod +x /config/scripts/video-processing.bash
+  chmod 777 /config/scripts/video-processing.bash
   echo "done"
 fi
 if [ ! -f /config/scripts/AudioPostProcessing.bash ]; then
@@ -30,7 +80,7 @@ if [ ! -f /config/scripts/AudioPostProcessing.bash ]; then
 
   # Set Permissions
   echo "setting permissions..."
-  chmod +x /config/scripts/AudioPostProcessing.bash
+  chmod 777 /config/scripts/AudioPostProcessing.bash
   echo "done"
 fi
 echo "script downloads complete..."
