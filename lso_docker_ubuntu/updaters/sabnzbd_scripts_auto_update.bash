b1c5f5b1c7e64f5cc8bf5ad37d0842cdd3cfd009
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-16 06:37:51 -0500

    Create sabnzbd_scripts_auto_update.bash

---
+++ lso_docker_ubuntu/updaters/sabnzbd_scripts_auto_update.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "==========START SABNZBD SCRIPTS AUTOMATED UPDATES==========="
+
+if [ -f /config/custom-cont-init.d/sabnzbd_script_setup.bash ]; then
+	echo "Previous version detected..."
+	echo "removing....sabnzbd_script_setup.bash"
+	rm /config/custom-cont-init.d/sabnzbd_script_setup.bash
+else 
+	echo "begining updated script installation..."
+	echo "downloading lidarr_dl_auto_installer.bash from: https://github.com/RandomNinjaAtk/Scripts/blob/master/lso_docker_ubuntu/sabnzbd_script_setup.bash"  && \
+	curl -o /config/custom-cont-init.d/sabnzbd_script_setup.bash https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/lso_docker_ubuntu/sabnzbd_script_setup.bash && \
+	echo "download complete" && \
+	echo "running sabnzbd_script_setup.bash..." && \
+	bash /config/custom-cont-init.d/sabnzbd_script_setup.bash && \
+	rm /config/custom-cont-init.d/sabnzbd_script_setup.bash
+fi
+echo "==========END SABNZBD SCRIPTS AUTOMATED UPDATES==========="
+exit 0
