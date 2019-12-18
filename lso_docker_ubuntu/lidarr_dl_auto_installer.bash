e20cbf9f8d544f64baa4b87214a4950dfad202a0
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2019-12-18 08:26:41 -0500

    adding tags, updated file naming

--- lso_docker_ubuntu/lidarr_dl_auto_installer.bash
+++ lso_docker_ubuntu/lidarr_dl_auto_installer.bash
@@ -105,20 +105,32 @@ fi
 wget https://notabug.org/RemixDevs/DeezloaderRemix/archive/development.zip && \
 unzip development.zip && \
 rm development.zip && \
-sed -i "s/\"trackNameTemplate\": \"%artist% - %title%\"/\"trackNameTemplate\": \"%disc%%number% - %title%\"/g" "/deezloaderremix/app/default.json" && \
-sed -i "s/\"albumTrackNameTemplate\": \"%number% - %title%\"/\"albumTrackNameTemplate\": \"%disc%%number% - %title%\"/g" "/deezloaderremix/app/default.json" && \
-sed -i "s/\"playlistTrackNameTemplate\": \"%position% - %artist% - %title%\"/\"playlistTrackNameTemplate\": \"%disc%%position% - %title%\"/g" "/deezloaderremix/app/default.json" && \
-sed -i "s/\"albumNameTemplate\": \"%artist% - %album%\",/\"albumNameTemplate\": \"%artist% - %album% (%album_id%) (WEB)-DREMIX\",/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"trackNameTemplate\": \"%artist% - %title%\"/\"trackNameTemplate\": \"%disc%%number% - %title% %explicit%\"/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"albumTrackNameTemplate\": \"%number% - %title%\"/\"albumTrackNameTemplate\": \"%disc%%number% - %title% %explicit%\"/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"playlistTrackNameTemplate\": \"%position% - %artist% - %title%\"/\"playlistTrackNameTemplate\": \"%disc%%position% - %title% %explicit%\"/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"albumNameTemplate\": \"%artist% - %album%\",/\"albumNameTemplate\": \"%artist% - %album%  %explicit%(%album_id%) (WEB)-DREMIX\",/g" "/deezloaderremix/app/default.json" && \
 sed -i "s/\"embeddedArtworkSize\": 800,/\"embeddedArtworkSize\": 1000,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"localArtworkSize\": 1000,/\"localArtworkSize\": 1400,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"saveArtwork\": false,/\"saveArtwork\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/cover/folder/g" "/deezloaderremix/app/default.json" && \
 sed -i "s/\"createCDFolder\": true,/\"createCDFolder\": false,/g" "/deezloaderremix/app/default.json" && \
 sed -i "s/\"createSingleFolder\": false,/\"createSingleFolder\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"syncedlyrics\": false,/\"syncedlyrics\": true,/g" "/deezloaderremix/app/default.json" && \
 sed -i "s/\"\; \"/\" \/ \"/g" "/deezloaderremix/app/default.json" && \
 sed -i "s/\"removeAlbumVersion\": false,/\"removeAlbumVersion\": true,/g" "/deezloaderremix/app/default.json" && \
 sed -i "s/\"trackTotal\": false,/\"trackTotal\": true,/g" "/deezloaderremix/app/default.json" && \
 sed -i "s/\"discTotal\": false,/\"discTotal\": true,/g" "/deezloaderremix/app/default.json" && \
-sed -i "s/\"date\": true,/\"date\": false,/g" "/deezloaderremix/app/default.json" && \
-sed -i "s/\"isrc\": true,/\"isrc\": false,/g" "/deezloaderremix/app/default.json" && \
-sed -i "s/\"publisher\": true,/\"publisher\": false,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"explicit\": false,/\"explicit\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"barcode\": false,/\"barcode\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"unsynchronisedLyrics\": false,/\"unsynchronisedLyrics\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"copyright\": false,/\"copyright\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"musicpublisher\": false,/\"musicpublisher\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"composer\": false,/\"composer\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"mixer\": false,/\"mixer\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"author\": false,/\"author\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"writer\": false,/\"writer\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"engineer\": false,/\"engineer\": true,/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"producer\": false/\"producer\": true/g" "/deezloaderremix/app/default.json" && \
 
 cd /deezloaderremix && \
 npm install && \
