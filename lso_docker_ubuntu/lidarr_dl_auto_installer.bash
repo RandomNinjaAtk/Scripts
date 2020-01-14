2861e5a15103bfbf96b7dc6fe82bf32b71e1b40e
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-01-13 21:22:25 -0500

    Update lidarr_dl_auto_installer.bash

--- lso_docker_ubuntu/lidarr_dl_auto_installer.bash
+++ lso_docker_ubuntu/lidarr_dl_auto_installer.bash
@@ -26,24 +26,56 @@ apt-get install -qq -y \
 apt-get purge --auto-remove -y && \
 apt-get clean
 
-mkdir /temp
-cd /temp
-git -C opus pull 2> /dev/null || git clone --depth 1 https://github.com/xiph/opus.git && \
-cd opus && \
-./autogen.sh && \
-./configure && \
-make && \
-make install
-cd /
+# Install packages needed
+ 
+apt update > /dev/null 2>&1 && apt install -y curl libflac-dev > /dev/null 2>&1
 
-cd /temp 
-git -C opus-tools pull 2> /dev/null || git clone https://gitlab.xiph.org/xiph/opus-tools.git && \
-cd opus-tools
-./autogen.sh && \
-./configure && \
-make && \
-make install
-cd /
+# Remove packages that can cause issues
+
+apt -y purge opus* > /dev/null 2>&1 && apt -y purge libopus-dev > /dev/null 2>&1
+
+# Download necessary files
+
+TEMP_FOLDER="$(mktemp -d)"
+
+# Opusfile 0.11
+curl -Ls https://downloads.xiph.org/releases/opus/opusfile-0.11.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+# Opus 1.3.1
+curl -Ls https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+# Libopusenc 0.2.1
+curl -Ls https://archive.mozilla.org/pub/opus/libopusenc-0.2.1.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+# Opus Tools 0.2
+curl -Ls https://archive.mozilla.org/pub/opus/opus-tools-0.2.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+# Compile
+
+cd "$TEMP_FOLDER"/opus-1.3.1 || exit
+
+./configure
+make && sudo make install
+
+cd "$TEMP_FOLDER"/opusfile-0.11 || exit
+
+./configure
+make && sudo make install
+
+cd "$TEMP_FOLDER"/libopusenc-0.2.1 || exit
+
+./configure
+make && sudo make install
+
+cd "$TEMP_FOLDER"/opus-tools-0.2 || exit
+./configure
+make
+sudo make install
+sudo ldconfig
+
+# Cleanup
+
+rm -rf "$TEMP_FOLDER"
 
 pip install --no-cache-dir -U \
 	beets \
