f0926abc43d11ef10c3913f7db14aba28c3e5552
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-01-20 08:58:18 -0500

    Update lsio-automated-installer.bash

--- lidarr-automated-downloader/docker/lsio-automated-installer.bash
+++ lidarr-automated-downloader/docker/lsio-automated-installer.bash
@@ -200,6 +200,13 @@ if [ -x "$(command -v crontab)" ]; then
 		echo "*/15 * * * *   root   bash /config/scripts/lidarr-automated-downloader-start.bash > /config/scripts/cron-job.log" >> "/etc/crontab"
 		service cron restart
 	fi
+	if grep "musicbrainzerror.log" /etc/crontab; then
+		echo "job already added..."
+	else
+		echo "adding cron job to crontab..."
+		echo "0 */12 * * *   root   rm "/config/scripts/lidarr-automated-downloader/musicbrainzerror.log"
+		service cron restart
+	fi
 else
 	echo "cron NOT INSTALLED"
 fi
