41b59198117a3864f2a943276af2d4c51e74e9ec
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-02-16 11:36:40 -0500

    Add opusenc tool, cleanup installation process, prevent overwrite of beets config file

--- lso_docker_ubuntu/sabnzbd_script_setup.bash
+++ lso_docker_ubuntu/sabnzbd_script_setup.bash
@@ -9,17 +9,98 @@ apt-get install -qq -y \
 	wget \
 	nano \
 	unzip \
-	jq \
-	libchromaprint-tools \
-	python-dev \
-	python-pip && \
+	jq && \
 apt-get purge --auto-remove -y && \
 apt-get clean
 
-pip install --no-cache-dir -U \
-	beets \
-	pyacoustid
-	
+if [ ! -f "/usr/local/bin/beet" ]; then
+	apt-get update -qq && \
+	apt-get install -qq -y \
+		libchromaprint-tools \
+		python3-pip && \
+	apt-get purge --auto-remove -y && \
+	apt-get clean
+
+	pip3 install --no-cache-dir -U \
+		beets \
+		pyacoustid
+else
+	echo "BEETS ALREADY INSTALLED"
+fi
+
+if [ ! -f "/usr/local/bin/opusenc" ]; then 
+	apt-get update -qq && \
+	apt-get install -qq -y \
+		autoconf \
+		automake \
+		libtool \
+		gcc \
+		make \
+		pkg-config \
+		openssl \
+		libssl-dev && \
+	apt-get purge --auto-remove -y && \
+	apt-get clean
+
+	set -e 
+	set -o pipefail
+
+	# Install packages needed
+
+	apt update > /dev/null 2>&1 && apt install -y curl libflac-dev > /dev/null 2>&1
+
+	# Remove packages that can cause issues
+
+	apt -y purge opus* > /dev/null 2>&1 && apt -y purge libopus-dev > /dev/null 2>&1
+
+	# Download necessary files
+
+	TEMP_FOLDER="$(mktemp -d)"
+
+	# Opusfile 0.11
+	curl -Ls https://downloads.xiph.org/releases/opus/opusfile-0.11.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+	# Opus 1.3.1
+	curl -Ls https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+	# Libopusenc 0.2.1
+	curl -Ls https://archive.mozilla.org/pub/opus/libopusenc-0.2.1.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+	# Opus Tools 0.2
+	curl -Ls https://archive.mozilla.org/pub/opus/opus-tools-0.2.tar.gz | tar xz -C "$TEMP_FOLDER"
+
+	# Compile
+
+	cd "$TEMP_FOLDER"/opus-1.3.1 || exit
+
+	./configure
+	make && make install
+
+	cd "$TEMP_FOLDER"/opusfile-0.11 || exit
+
+	./configure
+	make && make install
+
+	cd "$TEMP_FOLDER"/libopusenc-0.2.1 || exit
+
+	./configure
+	make && make install
+
+	cd "$TEMP_FOLDER"/opus-tools-0.2 || exit
+	./configure
+	make
+	make install
+	ldconfig
+
+	# Cleanup
+
+	rm -rf "$TEMP_FOLDER"
+
+	cd /
+else
+	echo "OPUSENC ALREADY INSTALLED"
+fi
+
 if ! [ -x "$(command -v ffmpeg)" ]; then
 	echo "INSTALLING FFMPEG"
 	apt-get update -qq && \
@@ -49,14 +130,9 @@ if [ ! -d /config/scripts ]; then
 fi
 
 if [ ! -d /config/scripts/beets ]; then
-	mkdir /config/scripts/beets
+	mkdir -p /config/scripts/beets
 fi
 
-
-if [ -f /config/scripts/beets/config.xml ]; then
-	rm /config/scripts/beets/config.xml
-	sleep 1s
-fi
 if [ ! -f /config/scripts/beets/config.xml ]; then
 	echo "downloading config.yaml from: https://github.com/RandomNinjaAtk/Scripts/blob/master/config/config.yaml"
 	curl -o /config/scripts/beets/config.yaml https://raw.githubusercontent.com/RandomNinjaAtk/Scripts/master/config/config.yaml
@@ -68,7 +144,7 @@ fi
 echo "downloading scripts..."
 if [ -f /config/scripts/video-processing.bash ]; then
 	rm /config/scripts/video-processing.bash
-	sleep 1s
+	sleep 0.1
 fi
 if [ ! -f /config/scripts/video-processing.bash ]; then
   echo "downloading video-processing.bash from: https://github.com/RandomNinjaAtk/Scripts/blob/master/sabnzbd/video-processing.bash"
@@ -80,9 +156,10 @@ if [ ! -f /config/scripts/video-processing.bash ]; then
   chmod 777 /config/scripts/video-processing.bash
   echo "done"
 fi
+
 if [ -f /config/scripts/AudioPostProcessing.bash ]; then
 	rm /config/scripts/AudioPostProcessing.bash
-	sleep 1s
+	sleep 0.1
 fi
 if [ ! -f /config/scripts/AudioPostProcessing.bash ]; then
   echo "downloading AudioPostProcessing.bash from: https://github.com/RandomNinjaAtk/Scripts/tree/master/sabnzbd/AudioPostProcessing.bash"
